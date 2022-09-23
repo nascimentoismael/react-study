fdc59d3706004d5f76cfbdb8080a59b130037cb9
Ismael Nascimento <55846164+nascimentoismael@users.noreply.github.com>
2022-09-22 22:00:07 -0300

    update em ListaAluno.js

--- react-app-with-tailwind/src/ListaAlunos.js
+++ react-app-with-tailwind/src/ListaAlunos.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 
 const ListaAlunos = () => {
 
+  useEffect(()=>{atualizar()},[]);
+
   const [estado, setEstado] = useState(
     {
       carregado: false,
@@ -11,17 +13,53 @@ const ListaAlunos = () => {
     }
   );
 
-  const atualizar = () => {
-    if(estado.carregado){
-      return;
+  const [aluno, setAluno] = useState(
+    {
+      id: 0,
+      name: "",
+      email: "",
+      phone: ""
     }
+  );
 
-    axios.get('http://localhost:3004/aluno')
+  const [show, setShow] = useState(false);
+
+  const atualizar = () => {
+    // if(estado.carregado){
+    //   return;
+    // }
+
+    axios.get('http://localhost:3000/aluno')
     .then(resp => {
       setEstado({carregado: true, dados: resp.data})
     }).catch(erro => {
       console.log(erro)
     })
+  }
+
+  const salvar=()=>{
+    if(aluno.id!==0){
+      axios.put(`http://localhost:3000/aluno/${aluno.id}`,aluno).catch((erro)=>console.log(erro))  
+    }else{
+      axios.post('http://localhost:3000/aluno',aluno).catch((erro)=>console.log(erro))
+    }
+  }
+
+  const excluir=(e)=>{
+    const id = parseInt(e.target.id);
+    axios.delete(`http://localhost:3000/aluno/${id}`)
+    .then(()=>atualizar())
+    .catch((erro)=>console.log(erro))
+  }
+
+  const editar=(e)=>{
+    const id = parseInt(e.target.id);
+    axios.get(`http://localhost:3000/aluno/${id}`)
+    .then((aluno)=>{
+      setAluno(aluno.data);
+      setShow(true);
+    })
+    .catch((erro)=>console.log(erro))
   }
 
   return (
@@ -34,7 +72,7 @@ const ListaAlunos = () => {
         </p>
       </div>
       <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-        <button
+        <button onClick={()=>setShow(!show)}
           type="button"
           class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
         >
@@ -69,6 +107,9 @@ const ListaAlunos = () => {
                       (
                         <tr key={x.id}>
                           <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                            {x.id}
+                          </td>
+                          <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                             {x.name}
                           </td>
                           <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
@@ -79,11 +120,13 @@ const ListaAlunos = () => {
                           </td>
                           <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                             <a href="#" class="text-indigo-600 hover:text-indigo-900">
-                              <i class="fa-solid fa-user-pen"></i>
+                              <i  id={x.id} onClick={editar} class="fa-solid fa-user-pen"></i>
                             </a>
-                            <a href="#" class="ml-2.5 text-indigo-600 hover:text-red-600">
-                              <i class="fa-solid fa-trash"></i>
-                            </a>
+                            <button class="ml-2.5 text-indigo-600 hover:text-red-600">
+                            
+                              <i  id={x.id} onClick={excluir}  class="fa-solid fa-trash"></i>
+                            
+                            </button>
                           </td>
                         </tr>
                       )
@@ -91,7 +134,29 @@ const ListaAlunos = () => {
                   }
               </tbody>
             </table>
+            
+            {
+              show && (<form style={{position: "relative", right: "60vh"}}>
+                <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+                  <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Cadastro do aluno</h2>
+                  <div class="relative mb-4">
+                    <label for="full-name" class="leading-7 text-sm text-gray-600">Nome Completo</label>
+                    <input onChange={(e)=> {setAluno({...aluno,name:e.target.value})}} type="text" value={aluno.name} id="full-name" name="full-name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                  </div>
+                  <div class="relative mb-4">
+                    <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+                    <input onChange={(e)=> {setAluno({...aluno,email:e.target.value})}} type="email" value={aluno.email} id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                  </div>
+                  <div class="relative mb-4">
+                    <label for="celular" class="leading-7 text-sm text-gray-600">Celular</label>
+                    <input onChange={(e)=> {setAluno({...aluno,phone:e.target.value})}} type="text" value={aluno.phone} id="celular" name="celular" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                  </div>
+                  <button onClick={salvar} class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Salvar</button>
+                </div>
+              </form>)
+            }
           </div>
+          {/* E.prevent reload */}
         </div>
       </div>
     </div>
